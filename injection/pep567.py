88206e72d209d0efbbb5595ada3d9ed05c46eda0
bswck <bartoszpiotrslawecki@gmail.com>
2024-10-21 01:00:35 +0200

    Use `cv.get` directly in `pep567_injection`

--- injection/pep567.py
+++ injection/pep567.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from functools import partial
 from typing import TYPE_CHECKING
 
 from injection.main import injection
@@ -14,14 +13,9 @@ if TYPE_CHECKING:
     T = TypeVar("T")
 
 
-def pep567_factory(cv: ContextVar[T], scope: Locals) -> T:  # noqa: ARG001
-    return cv.get()
-
-
 def pep567_injection(
     *aliases: str,
     cv: ContextVar[T],
     into: Locals | None = None,
 ) -> Injection[T]:
-    factory = partial(pep567_factory, cv)
-    return injection(*aliases, into=into, factory=factory, dynamic=True)
+    return injection(*aliases, into=into, factory=cv.get, dynamic=True)
